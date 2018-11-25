378d950f62a3b9e6dcfdb2832f73e9de0d1f8362
wangjy <wangjy@cotek-robotics.com>
2018-11-25 19:18:43 +0800

    dataBaseInit

--- STM32F7/USER/Data_Base.c
+++ STM32F7/USER/Data_Base.c
@@ -2,12 +2,35 @@
 
 Feedback_Msg  db_feedbackMsg = {0};
 Control_Msg   db_controlMsg =  {0};
+
 ROBOT_Input   g_ROBOT_Input =  {0};
 ROBOT_Output  g_ROBOT_Output = {0};
 
-void Update_CmdMsg(const SerialPakage msg)
+void dataBaseInit(void)
 {
-    memcpy(&db_controlMsg, msg.byData_, sizeof(Control_Msg)); 
+    db_feedbackMsg.now_angle = 0.0;
+    db_feedbackMsg.now_speed = 0.0;
+    
+    db_controlMsg.angle = 0.0;
+    db_controlMsg.speed = 0.0;
+}
+
+void updataMessage(const SerialPakage pack)
+{
+    switch(pack.head_.dataId)
+    {
+        case CMD_IPC_COMMOND:  updateCmdMsg(pack);
+            break;
+        case CMD_RESET:
+            break;
+        case DEBUG_QT_COMMOND:
+            break;
+    }
+}
+
+void updateCmdMsg(const SerialPakage pack)
+{
+    memcpy(&db_controlMsg, pack.byData_, sizeof(Control_Msg)); 
 }
 
 //void Update_FeedbackMsg(const Feedback_Msg msg)
